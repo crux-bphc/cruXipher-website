2e6edbbfdcf4f019f0043757c94118f8a5d1a063
pnicto <thehulk489@gmail.com>
2022-10-20 17:35:33 +0530

    Add outlet
    Now the navbar remains on all the pages

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,23 +1,29 @@
 import { useState } from "react";
+import { Link, Outlet } from "react-router-dom";
 import LinkButton from "./LinkButton";
 
 const Navbar = () => {
   const [points, setPoints] = useState(0);
 
   return (
-    <nav className="flex justify-between items-center px-6 py-2">
-      <div className="left-nav flex items-center gap-8">
-        <h2 className="font-bold text-3xl">cruXipher</h2>
-        <LinkButton linkText="about" url="#" />
-        <LinkButton linkText="rules" url="#" />
-        <LinkButton linkText="leaderboard" url="#" />
-      </div>
-      <div className="right-nav flex items-center gap-4">
-        <a className="text-white text-[20px]">{points} points</a>
-        <a className="text-white text-[20px]">timer</a>
-        <LinkButton linkText="logout" url="#" textColor="text-red" />
-      </div>
-    </nav>
+    <>
+      <nav className="flex justify-between items-center px-6 py-2">
+        <div className="left-nav flex items-center gap-8">
+          <Link className="font-bold text-3xl" to="/">
+            cruXipher
+          </Link>
+          <LinkButton linkText="about" url="about" />
+          <LinkButton linkText="rules" url="rules" />
+          <LinkButton linkText="leaderboard" url="leaderboard" />
+        </div>
+        <div className="right-nav flex items-center gap-4">
+          <a className="text-white text-[20px]">{points} points</a>
+          <a className="text-white text-[20px]">timer</a>
+          <LinkButton linkText="logout" url="#" textColor="text-red" />
+        </div>
+      </nav>
+      <Outlet />
+    </>
   );
 };
 
