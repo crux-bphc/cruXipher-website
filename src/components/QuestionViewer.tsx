b45cc381e13ada06e932d1c8fd0cc9e9cab55002
soumitradev <soumitra.shewale.dev@gmail.com>
2022-10-21 07:41:09 +0530

    Polish QuestionViewer

--- src/components/QuestionViewer.tsx
+++ src/components/QuestionViewer.tsx
@@ -3,11 +3,45 @@ import Question from "../types/Question";
 
 const QuestionView = () => {
   let params = useParams();
-  let todo = useLoaderData() as Question;
+  let question = useLoaderData() as Question;
   return (
     <>
-      <span>Question: {params.slug}</span>
-      <code>{JSON.stringify(todo)}</code>
+      {/* <span>Real slug: {params.slug}</span> */}
+      <div className="pl-16 pr-8 flex flex-col">
+        <span className="text-3xl font-bold">
+          {question.title} [{question.points} points]
+        </span>
+        <p className="text-xl font-normal pt-4 block">
+          {question.body.split("\n").map((x) => (
+            <>
+              {x}
+              <br />
+              <br />
+            </>
+          ))}
+        </p>
+        <div className="overflow-hidden">
+          <button className="border-white border-2 rounded-none bg-black h-16 py-4 float-right px-12 focus:outline-none text-xl ml-8 hover:bg-grey/10 transition-all ease-in-out duration-300">
+            SUBMIT
+          </button>
+          <span className="block overflow-hidden">
+            <input
+              className="border-white border-2 rounded-none w-full bg-black h-16 py-4 px-4 text-xl focus:outline-none focus:bg-grey/10 transition-all ease-in-out duration-300"
+              placeholder="Enter flag here"
+            />
+          </span>
+        </div>
+        <button className="border-white border-2 group rounded-none bg-black h-16 my-8 px-4 font-bold text-xl focus:outline-none hover:bg-grey/10 transition-all ease-in-out duration-300">
+          <div className="flex">
+            <span className="float-left py-4">Click here to get a hint</span>
+            <div className="grow"></div>
+            <span className="material-symbols-rounded float-right inline-flex items-center text-2xl py-2 group-hover:text-3xl transition-all ease-in-out duration-300">
+              arrow_forward_ios
+            </span>
+          </div>
+        </button>
+      </div>
+      {/* <code>{JSON.stringify(question)}</code> */}
     </>
   );
 };
--- src/pages/QuestionPage.tsx
+++ src/pages/QuestionPage.tsx
@@ -1,7 +1,16 @@
-import QuestionView from "../components/QuestionViewer";
+import QuestionViewer from "../components/QuestionViewer";
 
 const QuestionPage = () => {
-  return <QuestionView />;
+  return (
+    <div className="flex py-16">
+      <div className="min-w-4/6">
+        <QuestionViewer />
+      </div>
+      <div className="pr-16 pl-8 max-w-2/6 border-l-2 border-dashed border-opacity-30 border-white">
+        {"This is where the other questions should go. ".repeat(5)}
+      </div>
+    </div>
+  );
 };
 
 export default QuestionPage;
