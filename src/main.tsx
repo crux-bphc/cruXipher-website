2a908835025c82706bdb02e5d0177432a87dce3b
soumitradev <soumitra.shewale.dev@gmail.com>
2022-10-21 19:37:11 +0530

    Add Mantine paginator

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import { MantineProvider } from "@mantine/core";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
@@ -5,7 +6,14 @@ import App from "./App";
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <div className="min-h-screen bg-black text-white">
-      <App />
+      {" "}
+      <MantineProvider
+        theme={{ colorScheme: "dark" }}
+        withGlobalStyles
+        withNormalizeCSS
+      >
+        <App />{" "}
+      </MantineProvider>
     </div>
   </React.StrictMode>
 );
--- src/pages/LeaderBoard.tsx
+++ src/pages/LeaderBoard.tsx
@@ -1,5 +1,12 @@
+import { Pagination } from "@mantine/core";
+
 const LeaderBoard = () => {
-  return <div>LeaderBoard</div>;
+  return (
+    <div>
+      LeaderBoard
+      <Pagination size="xl" total={20} siblings={1} initialPage={1} withEdges />
+    </div>
+  );
 };
 
 export default LeaderBoard;
